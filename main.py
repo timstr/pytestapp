a621139b7d3eed303424dc5866e641dfd28e628a
timstr <straubinger.tim@gmail.com>
2021-12-10 16:07:25 -0800

    Added submodule

---
+++ main.py
@@ -0,0 +1,13 @@
+import pytestlib
+import pytestlib.operations as ops
+
+
+def main():
+    arr = pytestlib.make_array()
+    print(arr)
+    arr = ops.sort(arr)
+    print(arr)
+
+
+if __name__ == "__main__":
+    main()
